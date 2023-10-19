03cc666d2cd2266e5f34b69eab4bcba4d3475163
blackwhissh <blackwhissh@gmail.com>
2023-10-19 22:16:21 +0400

    Final Commit

--- src/main/java/com/epam/rd/autotasks/springemployeecatalog/exception/EmployeeNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.epam.rd.autotasks.springemployeecatalog.exception;
-
-public class EmployeeNotFoundException extends RuntimeException{
-    public EmployeeNotFoundException(Long employeeId){
-        super("Employee not found with ID: " + employeeId);
-    }
-}
